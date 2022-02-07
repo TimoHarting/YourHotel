4a7122ec61b43ccea4067b9371956174c4035ffd
TimoHarting <45542135+TimoHarting@users.noreply.github.com>
2022-02-07 17:23:52 +0100

    Spa objekt

---
+++ YourHotel/objects/spa/spaObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourHotel.objects.spa
+{
+    public class spaObject
+    {
+        public static DataSet Load()
+        {
+            DataSet dset = new DataSet();
+
+            string query = $"Select bezeichnung, preis From spa";
+            dset = db.dbconnection.Readout(query);
+
+            return dset;
+        }
+    }
+}
